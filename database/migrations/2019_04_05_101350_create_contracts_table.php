577ab90aaf1a09b1d3efdaa887eba14262ae7a49
anonymous <anonymous@example.com>
2019-04-05 12:54:29 +0200

    Create migrations

---
+++ database/migrations/2019_04_05_101350_create_contracts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('energy');
+            $table->string('product');
+            $table->string('mobile');
+            $table->unsignedInteger('duration');
+            $table->string('promo');
+            $table->timestamps();
+            $table->unsignedInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
